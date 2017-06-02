5afc00cb399b749472035c02cc705973177bc46b
Maurice Laveaux <mauricelaveaux@gmail.com>
2017-06-02 17:41:19 +0200

    Removed more unusused imports.

--- Project/PostProcessFX/Config/AntiAliasingConfig.cs
+++ Project/PostProcessFX/Config/AntiAliasingConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PostProcessFX.Config
+﻿namespace PostProcessFX.Config
 {
     /**
      * These settings are important for anti aliasing. 
