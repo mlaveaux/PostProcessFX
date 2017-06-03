7c9cee6ef9e857efa63251aafc2ad63a4ff0a023
Maurice Laveaux <mauricelaveaux@gmail.com>
2017-06-03 18:49:32 +0200

    Added a slider with a textbox that shows the current value.

--- Project/PostProcessFX/Utility.cs
+++ Project/PostProcessFX/Utility.cs
@@ -10,7 +10,31 @@ namespace PostProcessFX
         public static float drawSliderWithLabel(float x, float y, float min, float max, String label, float configValue)
         {
             GUI.Label(new Rect(x, y, 200, 20), label);
-            return GUI.HorizontalSlider(new Rect(x + 200, y + 5, 100, 20), configValue, min, max);
+            
+            float newValue = GUI.HorizontalSlider(new Rect(x + 200, y + 5, 100, 20), configValue, min, max);
+
+            if (newValue == configValue)
+            {
+                // The value has not changed, but might edit the text field.
+                string value = GUI.TextField(new Rect(x + 150, y, 30, 20), configValue.ToString());
+
+                // Read the value from the text field
+                if (float.TryParse(value, out newValue))
+                {
+                    // Check if new value lies with the ranges
+                    newValue = Math.Max(newValue, min);
+                    newValue = Math.Min(newValue, max);
+                }
+            }
+            else
+            {
+                // Just show the new value
+                string value = GUI.TextField(new Rect(x + 150, y, 30, 20), configValue.ToString());
+            }
+
+
+
+            return newValue;
         }
 
         public static float drawSliderWithLabel(float x, float y, float xsize, float ysize, float min, float max, String label, float configValue)
@@ -22,6 +46,14 @@ namespace PostProcessFX
         public static int drawIntSliderWithLabel(float x, float y, int min, int max, String label, int configValue)
         {
             GUI.Label(new Rect(x, y, 200, 20), label);
+            return (int)GUI.HorizontalSlider(new Rect(x + 200, y + 5, 100, 20), configValue, min, max);
+        }
+
+        public static int drawIntSliderWithLabel(float x, float y, int min, int max, String label, String valueLabel, int configValue)
+        {
+            GUI.Label(new Rect(x, y, 100, 20), label);
+            GUI.Label(new Rect(x + 150, y, 100, 20), valueLabel);
+
             return (int)GUI.HorizontalSlider(new Rect(x + 200, y + 5, 100, 20), configValue, min, max);
         }
 
